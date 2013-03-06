cfbda59a5b45c4291a0b79173da1b7346c58cfb3
TheBr0ken <jtoro2716@gmail.com>
2013-03-06 08:15:48 -0600

    Update vigor.mk

--- products/vigor.mk
+++ products/vigor.mk
@@ -2,10 +2,13 @@
 $(call inherit-product, device/htc/vigor/vigor.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common.mk)
 
 # Inherit CDMA common stuff
 $(call inherit-product, vendor/aokp/configs/cdma.mk)
+
+# Inherit common Verizon Wireless Perms and Lib
+$(call inherit-product, vendor/aokp/configs/vzw.mk)
 
 # Setup device specific product configuration.
 PRODUCT_NAME := aokp_vigor
@@ -14,7 +17,7 @@ PRODUCT_DEVICE := vigor
 PRODUCT_MODEL := ADR6425LVW
 PRODUCT_MANUFACTURER := HTC
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vigor BUILD_ID=IML74K BUILD_FINGERPRINT="verizon_wwe/htc_vigor/vigor:4.0.3/IML74K/372320.10:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vigor BUILD_ID=JDQ39 BUILD_FINGERPRINT="verizon_wwe/htc_vigor/vigor:4.4.2/JDQ39/134857:user/release-keys"
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip
